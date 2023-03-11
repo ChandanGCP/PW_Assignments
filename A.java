10557417675b2a5e2ef50ecc37c40fde21c982a8
ChandanGCP <codingworld32@gmail.com>
2023-03-11 11:13:44 +0530

    firstcommit

---
+++ A.java
@@ -0,0 +1,37 @@
+Q1 What iI Git ?
+  Git is a software
+  this software is also called distributed version control system .
+
+Q2  What do you understand by the term 'version control system' ?
+    It is a system that records changes to a file or set of files over time, so that we can recall specific versions later. 
+
+Q3  What is GitHub ?
+    GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.
+
+Q4   Maintion some popular Git hoIting servers ?
+     Github, Gitlab, BitBu4ket, Gitblit etc.
+
+Q5  Different type of version control system ?
+
+    There are 3 types of VCS
+i. Local Version CoItrol System (LVCS)
+ii. Centralised version Control System (CVCS)
+iii. Distributed Version Control System (DVCS)
+
+Q6  What bentfits come with using GIT ?
+    Developers does not check out the latest snapshot of the files rather they fully mirror the repository including its 
+full history.
+ If the main server dies, then the local system will maintain a copy of the main repository which has full 
+ba4kup of data.
+ If the remote repository is down, then the developer can make changes in the local repository and when the 
+main repository is up the code can be pushed to remote repository from local repository.
+
+
+Q7  What is a Git repoIitory?
+A Git repository is the . git/ folder inside a project. This repository tracks all changes made to files in your project,
+ building a history over time. Meaning, if you delete the . git/ folder, then you delete your project's history.
+
+Q8 How can you initialize a reposetory in in git ?
+  To create a new repo, you'll use the git init command. git init is a one-time command you use during the initial setup of a new repo.
+ Executing this command will create a new .git subdirectory in your current working directory. This will also create a new main branch.
+    
